761f368cd29bb6d8cfd869621ec046febb6b96c4
Matthew Wettlaufer <therivenman@gmail.com>
2012-09-04 19:52:38 -0700

    Added Vintage mode toggle
    * Added toggle for Vintage mode and gave it a cool shortcut (iv)
    
    * Also turned Vintage mode off by default

--- Toggles.py
+++ Toggles.py
@@ -1,10 +1,17 @@
 import sublime
 import sublime_plugin
 
-
 class ToggleWhiteSpaceCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         settings = sublime.load_settings("Preferences.sublime-settings")
         white_space = "selection" if settings.get("draw_white_space", "selection") != "selection" else "all"
         settings.set("draw_white_space", white_space)
         sublime.save_settings("Preferences.sublime-settings")
+
+class ToggleVintageModeCommand(sublime_plugin.ApplicationCommand):
+    def run(self):
+        settings = sublime.load_settings("Preferences.sublime-settings")
+        vintage = ["Vintage"] if settings.get("ignored_packages", ["Vintage"]) != ["Vintage"] else []
+        settings.set("ignored_packages", vintage)
+        sublime.save_settings("Preferences.sublime-settings")
+
